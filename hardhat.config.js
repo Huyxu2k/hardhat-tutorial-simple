201ae4379722b14c2633d1bb50b53c19bb513250
Huyxu2K <nguyenvanhuy2712000@gmail.com>
2023-08-03 11:50:05 +0700

    add default networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  defaultNetwork:"hardhat",
   networks: {
     ganache:  {
       url: process.env.PROVIDER_URL,
